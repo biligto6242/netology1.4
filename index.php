b50a22b924550b4b367636c92d466c5f75ec9ec0
biligto6242 <biligto6242@yandex.ru>
2018-05-17 02:47:21 +0300

    12345

--- index.php
+++ index.php
@@ -13,13 +13,39 @@
         City:<input type="text" name="city" placeholder="city">
         <input type="submit" name="submit" placeholder="submit">
     </form>
+    <?php
+    $appid = '613262e22c6c2a8e8dc3bbe1950a3de3';
+    if(isset($_GET)){
+        $usercity = $_GET['city'];
+
+        $apiurl = "http://api.openweathermap.org/data/2.5/weather?q=$usercity&appid=$appid&lang=ru";
+        $weatherdata = file_get_contents($apiurl);
+        $json = json_decode($weatherdata, TRUE);
+
+        $temp = $json['main']['temp'];//temperature
+        $humidity = $json['main']['humidity'];//humidity
+        $condition = $json['weather'][0]['main'];//weather condition
+        $wind = $json['wind']['speed'];//wind speed
+        $direction = $json['wind']['deg'];//wind direction
+
+        echo"<strong> City: </strong>" . $usercity . "<br>";
+        echo"<strong> Humidity: </strong>" . $humidity . "<br>";
+        echo"<strong> Current condition: </strong>" . $condition . "<br>";
+        echo"<strong> Wind speed: </strong>" . $wind . "<br>";
+        echo"<strong> Wind direction </strong>" . $direction . "<br>";
+        echo"<strong> Temperature: </strong>" . $temp . "<br>";
+
+    };
+    ?>
+
+    <?php
+    if(isset($_GET['submit'])){
+        $data = "data.json";
+        $json_string = json_encode($_GET, JSON_PRETTY_PRINT);
+        file_put_contents($data, $json_string, FILE_APPEND);
+    };
+    ?>
 </body>
 </html>
-<?php
-if(isset($_GET)){
-    $user_city = $_GET['city'];
 
-    $api_url = "api.openweathermap.org/data/2.5/weather?q=" . $user_city;
-    $weather_data = file_get_contents($api_url);
-    $json = json_decode($weather_data, TRUE);
-}
+
